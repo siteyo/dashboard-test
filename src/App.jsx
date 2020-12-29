41863f01e75156d8f05070c1fb847a16cc9f1890
siteyo <phicyanoc@gmail.com>
2020-12-29 18:18:10 +0900

    Update

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from './components/Layout';
+import './normalize.css';
+import './App.css';
+
+const App = () => {
+  return (
+    <Layout>
+      <h1>Dashboard</h1>
+    </Layout>
+  );
+};
+
+export default App;
