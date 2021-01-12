4c142870cc3bc0bc2b815533a33d3020b53d80d8
siteyo <phicyanoc@gmail.com>
2021-01-12 23:15:31 +0900

    LayoutにHeaderを追加

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Header from '../Header';
 import './style.css';
 
 const Layout = ({ children }) => {
-  return <div className="layout">{children}</div>;
+  return (
+    <div className="layout">
+      <h1>Dashboard</h1>
+      <Header />
+      {children}
+    </div>
+  );
 };
 
 Layout.propTypes = {
